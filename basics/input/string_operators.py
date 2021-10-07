6c5d5e7dc971b4764a3ff18b66b2c3214e081586
Dale <5elded03@solent.ac.uk>
2021-10-07 12:45:20 +0100

    adding code to demonstrate sting operators

---
+++ basics/input/string_operators.py
@@ -0,0 +1,6 @@
+# code to demonstrate the use of string operators #
+lives = input("Please enter the number of lives. :")
+energy = input("Please enter the energy level. :")
+shield = input("Please enter the shield level. :")
+print("")
+print("Health has been set.")
