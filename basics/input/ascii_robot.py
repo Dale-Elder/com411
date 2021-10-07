0e9cd4facaab3b7b251d2dee4185737604e3bad1
Dale <5elded03@solent.ac.uk>
2021-10-07 11:56:07 +0100

    Added code to display an ASCII robot with dynamic eye.

---
+++ basics/input/ascii_robot.py
@@ -0,0 +1,8 @@
+# Display Ascii Art #
+eye = input("Please pick a Character for the eyes:")
+mouth = input("Please pick a Character for the mouth:")
+print("\t\\/\t")
+print("##########")
+print(f"# \t{eye*2}\t #")
+print(f"#\t{mouth*3}\t #")
+print("##########")
