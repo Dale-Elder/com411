ed1e733cce16ab9d1243458e05c4e1dec9766c70
Dale <5elded03@solent.ac.uk>
2021-10-07 12:44:58 +0100

    adding code to demonstrate sting operators

--- basics/input/data_types.py
+++ basics/input/data_types.py
@@ -1,8 +1,8 @@
 # Code to demonstrate different uses of data types #
-name = input("What is your human name?")
-age = int(input("How old are you (in years)?"))
-height = float(input("How tall are you (in meters)?"))
-weight = float(input("How much do you weight? (in kilograms)?"))
+name = input("What is your human name?:")
+age = int(input("How old are you (in years)?:"))
+height = float(input("How tall are you (in meters)?:"))
+weight = float(input("How much do you weight? (in kilograms)?:"))
 bmi = weight / (height * height)
 
 print(f"{name}you are {age} years old and your bmi is {bmi}")
