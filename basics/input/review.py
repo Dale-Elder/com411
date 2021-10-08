7656ce4a0fd55a11fe32f6e2aa297234fb7737f7
Dale <5elded03@solent.ac.uk>
2021-10-08 10:12:49 +0100

    Created a basic input/output program.

---
+++ basics/input/review.py
@@ -0,0 +1,6 @@
+# Creating a basic input/output program #
+print("Hello, My name is Beep!")
+name = str(input("What's your name? "))
+print(f"Well hello there {name} it's nice to meet you!")
+print("\n\n\n")
+print("#"*15)
