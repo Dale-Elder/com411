c783decced344fc4c0bd5666ccbab1cf41a99e63
Dale <5elded03@solent.ac.uk>
2021-11-10 11:45:42 +0000

    Fixed overwrite issue

--- basics/functions/ascii_code.py
+++ basics/functions/ascii_code.py
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-# code to demonstrate the use of built-in functions: len, ord. #
-
-print("Program Started!")
-print("Please enter a standard character: ")
-char = input()
-
-for letter in char:
-    value = ord(char)
-    if len(char) == 1:
-        print(f"The ASCII code for {char} is {value}.")
-    else:
-        print("Unexpected input.")
-
-
-=======
 # code to demonstrate the use of built-in functions len, ord #
 
 print("Program started!\n")
@@ -24,5 +8,5 @@ if len(character) == 1:
     print(f"The ASCII code for {letter} is {value}")
 else:
     print("Oops!, that was not a valid, single character please try again.")
->>>>>>> 875e8831beffef9a49ec744fbae70b5a9c6c7c00
+
 print("Program Ended!")
