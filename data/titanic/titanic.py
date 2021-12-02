0a9bf16684fdf21d7ed6f8f985010a8af7bd1b54
Dale <5elded03@solent.ac.uk>
2021-12-02 14:24:12 +0000

    Added code to display names of passengers.

--- data/titanic/titanic.py
+++ data/titanic/titanic.py
@@ -25,15 +25,27 @@ Please select one of the following options:
 [4] Display the number of passengers per age group
 [5] Display the number of survivors per age group"      
         """)
-    user_response = int(input())
-    return user_response
+
+    return int(input())
+
+
+def display_passenger_names():
+    print("The names of the passengers are...\n")
+    for record in records:
+        passenger_name = record[3]
+        print(passenger_name)
+
 
 def run():
     load_data("titanic.csv")
     num_records = len(records)
     print(f"Successfully loaded {num_records} records.")
     selected_option = display_menu()
-    print(f"You have selected option: {selected_option}")
+    print(f"You have selected option: {selected_option}\n")
+    if selected_option == 1:
+        display_passenger_names()
+    else:
+        print("Error! Option not recognised!")
 
 
 if __name__ == "__main__":
